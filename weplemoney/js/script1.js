e85df4be00a301faac99346076a1a18438cccf3b
songjieun1 <eldmdz124@naver.com>
2023-06-07 17:22:37 +0900

    modify

--- weplemoney/js/script1.js
+++ weplemoney/js/script1.js
@@ -2,8 +2,8 @@ $(function() {
 
 
     // 메인이미지1 페이드인
-    $('#title').fadeIn('4000');
-    $('#mainimg').fadeIn('4000');
+    $('#title').fadeIn("slow");
+    $('#mainimg').fadeIn("slow");
     
 
     //  메인이미지2 스크롤 슬라이드 
@@ -13,7 +13,7 @@ $(function() {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            if( bottom_of_window > bottom_of_element ){
+            if( bottom_of_window > bottom_of_element/2 ){
                 $(this).animate({'margin-left':'0px'},2000);
             }
             
