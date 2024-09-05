c23d1aef079c7ce8d4dc18fcd9a4408f82ea86b3
songjieun1 <eldmdz124@naver.com>
2024-09-05 20:14:11 +0900

    modify html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
 $(function () {
 
+    //  스크롤 이벤트
+
+    window.addEventListener("wheel", function (e) {
+        e.preventDefault();
+    }, { passive: false });
+    //-------------------------------------------------------
+    // 한 페이지 단위로 스크롤 이동
+
+    var $html = $("html");
+
+    var page = 1;
+
+    var lastPage = $(".page").length;
+
+    $html.animate({scrollTop:0},10);
+
+    $(window).on("wheel", function (e) {
+
+        if($html.is(":animated")) return;
+
+        if(e.originalEvent.deltaY > 0) {
+            if(page == lastPage) return;
+
+            page++;
+        }
+        else if (e.originalEvent.deltaY < 0) {
+            if(page == 1) return;
+
+            page--;
+        }
+        var posTop = (page-1) * $(window).height();
+
+        $html.animate({scrollTop : posTop });
+
+    });
+
+
+
+    //  타이핑효과 
     var typingBool = false;
     var typingIdx = 0;
 
-    
+
     var typingTxt = $("#header > h3").text();
 
     typingTxt = typingTxt.split(""); // 한글자씩 나오게자름 
